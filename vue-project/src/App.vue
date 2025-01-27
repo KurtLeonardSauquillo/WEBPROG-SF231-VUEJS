e299e6b5e3cdfcbb8da2c2ae710b44a6ed3f3fc6
KurtLeonardSauquillo <152581258+KurtLeonardSauquillo@users.noreply.github.com>
2025-01-27 11:13:13 +0800

    Adding components

--- vue-project/src/App.vue
+++ vue-project/src/App.vue
@@ -7,3 +7,23 @@
 </template>
  
 <style></style>
+
+<template>
+    <h1>Food</h1>
+    <food-item/>
+    <food-item/>
+    <food-item/>
+  </template>
+
+
+<style>
+
+  #app > div {
+    border: dashed black 1px;
+    display: inline-block;
+    margin: 10px;
+    padding: 10px;
+    background-color: lightgreen;
+  }
+
+</style>
