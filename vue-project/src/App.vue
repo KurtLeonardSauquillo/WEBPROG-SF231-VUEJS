a6f9c79df5396957208dd63e346206bd985d03ca
KurtLeonardSauquillo <152581258+KurtLeonardSauquillo@users.noreply.github.com>
2025-03-10 23:18:11 +0800

    rest api

--- vue-project/src/App.vue
+++ vue-project/src/App.vue
@@ -1,10 +1,32 @@
-   <h1>Food</h1>
-  <personal-profile/>
-  <food-item/> 
-  <food-item2/>
-  <food-item/> 
-  <food-item2/> 
-  <rest-api/> 
+script setup>
+import { ref, onMounted } from 'vue'
+import { supabase } from './lib/supabaseClient'
+
+const countries = ref([])
+
+async function getCountries() {
+  const { data } = await supabase.from('countries').select()
+  countries.value = data
+}
+
+onMounted(() => {
+  getCountries()
+})
+</script>
+
+<template>
+<ul>
+  <li v-for="country in countries" :key="country.id">{{ country.name }}</li>
+</ul>
+
+
+ <h1>Food</h1>
+<personal-profile/> <br>
+<food-item/> 
+<food-item2/>
+<food-item/> 
+<food-item2/> 
+<rest-api/> 
 </template>
 
-<style></style>
+<style></style><
