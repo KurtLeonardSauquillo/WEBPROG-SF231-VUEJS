c5148c1b9bde2ccf17de3b2575dabd3be518bff0
KurtLeonardSauquillo <152581258+KurtLeonardSauquillo@users.noreply.github.com>
2025-03-10 15:16:00 +0800

    Added profile

---
+++ vue-project/src/components/PersonalProfile.vue
@@ -0,0 +1,70 @@
+<div class="container">
+    <h1>Personal Profile</h1>
+    <a href="https://imgbb.com/" target="_blank">
+        <img src="https://i.ibb.co/FYsVTdG/cat.jpg" alt="cat">
+    </a>
+    <p>
+        Hello! My name is <strong>Kurt Leonard A. Sauquillo</strong>. My name is Kurt, my friends call me by initials KL.  
+        I enjoy listening to music and playing instruments, as well as playing video games.  
+        I mainly play <strong>League of Legends</strong> at the moment, but I used to play games like <em>Valorant</em>, <em>Overwatch 2</em>, etc.
+    </p>
+    
+    <section id="educationSection">
+        <h2>Education & Achievements</h2>
+        <p>I went to Sto Niño Catholic School for my Junior and Senior High School. I am currently studying at Asia Pacific College.</p>
+    </section>
+
+    <section id="courseSection">
+        <h2>Course</h2>
+        <p>3rd year I.T. Student specializing in Mobile and Internet Technologies at Asia Pacific College.</p>
+    </section>
+
+    <section id="itExperienceSection">
+        <h2>IT Experience</h2>
+        <p>Things I have done related to I.T so far:</p>
+        <ol>
+            <li>Built my own PC from scratch.</li>
+            <li>Created an Arduino project that detects doors opening and closing using a motion sensor.</li>
+            <li>Troubleshooted my PC xD.</li>
+        </ol>
+    </section>
+
+    <section id="hobbiesSection">
+        <h2>Hobbies & Interests</h2>
+        <ul>
+            <li>Gaming</li>
+            <li>Not Coding</li>
+            <li>Listening to Music</li>
+            <li>Playing instruments</li>
+        </ul>
+    </section>
+
+    <section id="goalsSection">
+        <h2>Goals in Life</h2>
+        <p>
+            My goal is to graduate college and be done studying so I can finally get a job and get paid money.  
+            I also want to travel to different countries. The countries I want to visit are <strong>Japan, Jamaica, Thailand, and Korea</strong>.
+        </p>
+    </section>
+
+    <section id="gallerySection">
+        <h2>Picture Gallery</h2>
+        <div class="gallery">
+            <a href="https://ibb.co/2Ymj61B">
+                <img src="https://i.ibb.co/Hp1r2fQ/me-fr.jpg" alt="me-fr">
+            </a>
+            <a href="https://imgbb.com/">
+                <img src="https://i.ibb.co/fGLxq34/sonic.jpg" alt="sonic">
+            </a>
+            <a href="https://ibb.co/s1qHk29">
+                <img src="https://i.ibb.co/7pK48Xt/kobe.jpg" alt="kobe">
+            </a>
+            <a href="https://ibb.co/6nrPC6y">
+                <img src="https://i.ibb.co/h7LWzpg/coby.jpg" alt="coby">
+            </a>
+            <a href="https://imgbb.com/">
+                <img src="https://i.ibb.co/rscxfZH/sonic.jpg" alt="new-sonic">
+            </a>
+        </div>
+    </section>
+</div>
