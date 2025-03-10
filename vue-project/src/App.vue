3a96197ab222600d1ef9656e611fa83b6890f744
KurtLeonardSauquillo <152581258+KurtLeonardSauquillo@users.noreply.github.com>
2025-03-10 23:09:54 +0800

    rest

--- vue-project/src/App.vue
+++ vue-project/src/App.vue
@@ -1,13 +1,10 @@
-<template>
-    <h1>Food</h1>
-    <personal-profile/>
-    <food-item/> 
-    <food-item2/> 
-    <food-item/>  
-    <rest-api/>
-  </template>
+   <h1>Food</h1>
+  <personal-profile/>
+  <food-item/> 
+  <food-item2/>
+  <food-item/> 
+  <food-item2/> 
+  <rest-api/> 
+</template>
 
-
-<style>
-
-</style>
+<style></style>
